5e66610c5e4eee1f4002c97dbe0498876cb0354e
Vansh Saboo <vanshchessdude@gmail.com>
2024-06-07 14:38:59 -0400

    Functional code — can construct m x n wordboxes

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,11 @@ use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-/// IMPLEMENTATION LIST
-/// - Algorithm for finding asymmetrical square word boxes
-///     - Algorithm for finding rectangular word boxes
-/// Faster algorithmm e.g. A* search
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WordBox {
-    row_dim: usize,       // number of rows
-    col_dim: usize,       // number of columns
-    rows: Vec<String>,    // the words for each row
-    columns: Vec<String>, // the words for each column
+    row_dim: usize,    // number of rows
+    col_dim: usize,    // number of columns
+    rows: Vec<String>, // the words for each ROW
 }
 
 impl Display for WordBox {
@@ -22,12 +16,6 @@ impl Display for WordBox {
         for (i, word) in self.rows.iter().enumerate() {
             for (j, ch) in word.chars().enumerate() {
                 grid[i][j] = ch;
-            }
-        }
-
-        for (i, word) in self.columns.iter().enumerate() {
-            for (j, ch) in word.chars().enumerate() {
-                grid[j][i] = ch;
             }
         }
 
@@ -80,45 +68,45 @@ fn filter_words(filename: &str) -> Vec<String> {
 
 impl WordBox {
     fn is_done(&self) -> bool {
-        self.rows.len() == self.row_dim && self.columns.len() == self.col_dim
+        self.rows.len() == self.row_dim
     }
 
-    fn take_ith_characters(words: &Vec<&String>, i: usize) -> String {
+    fn take_ith_characters(words: &Vec<String>, i: usize) -> String {
         words
             .iter()
             .map(|word| word.chars().nth(i).unwrap())
             .collect()
     }
 
-    fn next_moves(&'b self, lexicon: &'b Lexicon) -> Vec<&'b String> {
+    fn next_moves(&self, lexicon: &Lexicon) -> Vec<String> {
         let mut moves = vec![];
-        let mut rows: Vec<&String> = self.rows.iter().collect();
+        let mut rows: Vec<String> = self.rows.clone();
         for word in lexicon.words_with_prefix(&"".to_string(), self.col_dim) {
-            rows.push(&word);
+            rows.push(word.clone());
             let mut flag: bool = true;
             for i in 0..self.col_dim {
                 let prefix = Self::take_ith_characters(&rows, i);
-                let choice = lexicon.words_with_prefix(&prefix, self.col_dim);
+                let choice = lexicon.words_with_prefix(&prefix, self.row_dim);
                 if choice.is_empty() {
                     flag = false;
                     break;
                 }
             }
             if flag {
-                moves.push(&word);
+                moves.push(word.clone());
             }
+            rows.remove(rows.len() - 1);
         }
         moves
     }
 
-    fn add_word(&self, word: &String) -> WordBox {
+    fn add_word(&self, word: String) -> WordBox {
         let mut rows: Vec<String> = self.rows.clone();
         rows.push(word.clone());
         WordBox {
             row_dim: self.row_dim,
             col_dim: self.col_dim,
             rows,
-            columns: self.columns.clone(),
         }
     }
 }
@@ -136,76 +124,25 @@ fn solve_word_box(wb: WordBox, lexicon: &Lexicon) -> Option<WordBox> {
     }
     None
 }
-
-/// Check if the words form a word box
-/// Current check requires that the words are all the same length, and that the grid is symmetric across the diagonal
-
-/*
-fn is_word_box(words: &[String]) -> bool {
-    if !words.iter().all(|word| word.len() == words.len()) {
-        return false;
-    }
-
-    // Check if the grid is symmetric
-    let len = words.len();
-    for i in 0..len {
-        for j in 0..len {
-            if words[i].chars().nth(j) != words[j].chars().nth(i) {
-                return false;
-            }
-        }
-    }
-
-    true
-}
-*/
-/*
-fn solve_word_box(words: &[String], box_size: usize, lexicon: &Lexicon) -> bool {
-    let len = words.len();
-
-    if len == box_size {
-        let word_box = WordBox {
-            words: words.to_vec(),
-        };
-        println!("{:}", word_box);
-        return true;
-    }
-
-    let prefix: String = words
-        .iter()
-        .map(|word| word.chars().nth(len).unwrap())
-        .collect();
-
-    let choices = lexicon.words_with_prefix(&prefix, box_size);
-
-    for choice in choices.iter() {
-        let mut new_words = words.to_owned();
-        new_words.push(choice.clone());
-        let result = solve_word_box(&new_words, box_size, lexicon);
-        if result {
-            return true;
-        }
-    }
-
-    false
-}
-*/
-
 fn main() {
     let words = filter_words("../3esl.txt");
     let lexicon = Lexicon { words };
 
-    let rows: Vec<String> = vec![];
-    let columns: Vec<String> = vec![];
-    let word_box = WordBox {
-        row_dim: 4,
-        col_dim: 4,
-        rows,
-        columns,
-    };
+    for i in 2..=4 {
+        for j in 2..=4 {
+            let rows: Vec<String> = vec![];
+            let word_box = WordBox {
+                row_dim: i,
+                col_dim: j,
+                rows,
+            };
 
-    println!("{}", lexicon.words.len());
-    println!("{:}", word_box);
+            let word_box_option = solve_word_box(word_box, &lexicon);
 
-    // println!("{:?}", solve_word_box(word_box, &lexicon));
+            match word_box_option {
+                Some(word_box) => println!("{}", word_box),
+                None => println!("No word box found.\n"),
+            }
+        }
+    }
 }
